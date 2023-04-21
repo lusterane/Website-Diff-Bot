50e2903dc53f6f52e89185e128cfb916792c577f
Toby Chow <tobychow98@gmail.com>
2023-04-21 10:12:38 -0500

    worksave api utils

--- Services/APIUtils.py
+++ Services/APIUtils.py
@@ -1,7 +1,7 @@
 import re
-from datetime import datetime, timezone
 
 from Models.DBGateway import *
+from Services.LoggerContext import logger
 
 
 class APIUtils:
@@ -35,18 +35,96 @@ class APIUtils:
         checks = job.scraped_data.checks
         return APIUtils.__jsonify_instrument_list__(checks)
 
+    '''
+    returns job that's created
+    '''
+
+    @staticmethod
+    def create_job(job_name, link, frequency, profile_id) -> {}:
+        APIUtils.__validate_job_before_create_job__(job_name, link, profile_id)  # raises exception if not validated
+
+        from Services.WebsiteScraper import WebsiteScraper
+        new_scraped_data = WebsiteScraper.scrape_link(link)
+
+        old_scraped_data_entity = ScrapedData.get_scraped_data_by_link(link)
+        if not old_scraped_data_entity:
+            new_scraped_data_entity = ScrapedData.create_scraped_data(scraped_data=new_scraped_data, link=link)
+
+        check = Check.create_check(status=Check.Status.FirstCheck, s_id=old_scraped_data_entity.s_id)
+        last_updated = check.checked_on
+        job_data = {
+            'job_name': job_name,
+            'frequency': frequency,
+            'last_updated': last_updated,
+            'next_update': APIUtils.__get_next_update_time__(last_updated=last_updated, frequency=int(frequency)),
+            'p_id': profile_id,
+            's_id': old_scraped_data_entity.s_id
+        }
+        return Job.create_job(job_data=job_data).__json__()
+
     ''' Helper Methods '''
+
+    @staticmethod
+    def __validate_job_before_create_job__(job_name, link, profile_id):
+        profile = Profile.get_profile_by_id(profile_id)
+        if not profile:
+            raise Exception(f'Profile ID {profile_id} does not exist')
+
+        # check if there's already a job with same job_name or link for a profile
+        for job in profile.jobs:
+            if job.job_name == job_name:
+                raise Exception(f'Job with job name {job_name} already exists')
+            elif job.scraped_data.link == link:
+                raise Exception(f'Job with link {link} already exists')
+
+    # pass job only if in update job flow !
+    # updates/inserts ScrapedData, Check, Job. if necessary, inserts Diff.
+    @staticmethod
+    def __refresh_all_tables__(job_name, link, frequency, profile_id, job: Job = None) -> Job:  # return time job is refreshed
+        # if job:
+        #     # UPDATE_JOB FLOW
+        #     old_scraped_data_entity = job.scraped_data.scraped_data  # will always exist
+        #     ScrapedData.update_scraped_data(s_id=old_scraped_data_entity.s_id, new_scraped_data=new_scraped_data)
+        # else:
+        # # CREATE_JOB FLOW
+        #
+        # s_id, old_scraped_data = old_scraped_data_entity.s_id, old_scraped_data_entity.scraped_data
+        #
+        # diff_string = APIUtils.__get_html_diff__(old_scraped_data, new_scraped_data)  # returns empty string if no diff
+        # if diff_string:
+        #     Diff.create_diff(diff_string, s_id)
+        #     try:
+        #         from Services.EmailManager import EmailManager
+        #         email_manager = EmailManager()
+        #         email_manager.test_send_email_success()
+        #         check = Check.create_check(status=Check.Status.AlertSent, s_id=s_id)
+        #     except Exception as e:
+        #         check = Check.create_check(status=Check.Status.SendAlertFailed, s_id=s_id)
+        #         logger.critical('Failed to send email Error:', e)
+        # else:
+        #     check = Check.create_check(status=Check.Status.NoChange, s_id=s_id)
+        # last_updated = check.checked_on
+        # job_data = {
+        #     'job_name': job_name,
+        #     'frequency': frequency,
+        #     'last_updated': last_updated,
+        #     'next_update': APIUtils.__get_next_update_time__(last_updated=last_updated, frequency=int(frequency)),
+        #     'p_id': profile_id,
+        #     's_id': old_scraped_data_entity.s_id
+        # }
+        # return Job.create_job(job_data=job_data).__json__()
+        return None
+
+    @staticmethod
+    def __get_next_update_time__(last_updated: datetime, frequency: int):
+        return last_updated + datetime.timedelta(minutes=frequency)
 
     @staticmethod
     def __jsonify_instrument_list__(lis):
         return [k.__json__() for k in lis]
 
     @staticmethod
-    def __get_current_time__() -> str:
-        return str(datetime.now(timezone.utc))
-
-    @staticmethod
-    def __get_html_diff(str1, str2) -> str:
+    def __get_html_diff__(str1, str2) -> str:
         # if there's no diff, give empty string
         if str1 == str2:
             return ''
